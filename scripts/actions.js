6be8d875c8d15f516a685186546f987b0f035a7b
MDGFUKAS <mguerrerofinol@gmail.com>
2022-08-21 05:35:46 -0400

    [FEAT]: CV's, whapsapp contact

--- scripts/actions.js
+++ scripts/actions.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => {
   const overlay = document.getElementsByClassName('overlay');
   const navBtns = document.getElementsByClassName('NavButtons');
+  const downloadBtns = document.getElementsByClassName('download_button')
+  const wsTextArea = document.getElementById('ws_contact_message');
 
   const coloseBtn = document.getElementsByClassName('close');
 
-  for (let i = 0; i <= navBtns.length -2; i++) {
+  for (let i = 0; i <= navBtns.length -1; i++) {
     navBtns[i].addEventListener('click', () => openModal(overlay[i]))
   }
   for (let n = 0; n <= coloseBtn.length - 1; n++) {
     coloseBtn[n].addEventListener('click', () => closeModal(overlay[n]));
   }
 
-  navBtns[4].addEventListener('click', () => downloadCV())
+  // navBtns[4].addEventListener('click', () => downloadCV())
+
+  downloadBtns[0].addEventListener('click', () => downloadCV('es'))
+  downloadBtns[1].addEventListener('click', () => downloadCV('en'))
+
+  wsTextArea.addEventListener('input', function () {
+    this.style.height = 'auto'
+    this.style.height = `${this.value === "" ? 40 : this.scrollHeight}px`;
+  }, false)
 })
 
 function openModal (elem, keyCode) {
@@ -22,11 +32,11 @@ function closeModal (elem, keyCode) {
   elem.style.display = "none"
 }
 
-function downloadCV(keyCode) {
-  const url = '../assets/files/Marcos_Guerrero_CV.pdf';
+function downloadCV(lang, keyCode) {
+  const url = `../assets/files/Marcos_Guerrero_CV-${lang}.pdf`;
   const link = document.createElement('a');
   link.href = url;
-  link.setAttribute('download', 'Marcos Guerrero CV.pdf');
+  link.setAttribute('download', `Marcos Guerrero CV ${lang.toUpperCase()}.pdf`);
   document.body.appendChild(link);
   link.click();
   link.parentNode.removeChild(link);
--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,27 @@ const en = {
     "Hello! I am Marcos, a self-taught and quite dedicated developer with +2 years of experience, I am focused on developing web and mobile applications with javascript, I am currently working at <a href='https://www.linkedin.com/company/neurons-agency/' rel='noopener noreferer' target='_blank'>@neurons_agency</a> as a mid-senior Front-end developer with react and javascript",
   ROLE: 'Front-end Developer',
   CONTACT_ME: {
-    labels: ['Your Name', 'Email', 'Why do you contact me?'],
-    send: 'Send',
-    subject: 'Hi! I need comunication with you.',
-    message: (name, email, message) =>
-      `Hello Marcos! \n\n${name} contact you from ${email} \n\nAnd says the following: \n${message || '(No body message)'}`,
+    EMAIL: {
+      labels: ['Your Name', 'Email', 'Why do you contact me?'],
+      send: 'Send',
+      subject: 'Hi! I need comunication with you.',
+      message: (name, email, message) =>
+        `Hello Marcos! \n\n${name} contact you from ${email} \n\nAnd says the following: \n${message || '(No body message)'}`,
+    },
+    WS: {
+      title: 'Write your message',
+      placeholder: 'Tell me anything!',
+    },
+    BUTTONS: ['Or write me on WhatsApp 👉', 'Or send me a message 👉'],
+    warning: 'Missing Fields',
+  },
+  DOWNLOAD_CV: {
+    title: 'Download CV',
+    btnEs: 'Download (ES)',
+    btnEn: 'Download (EN)',
+  },
+  FOOTER: {
+    title: 'My Networks',
   },
 };
 
@@ -20,11 +36,27 @@ const es = {
     "¡Hola! Soy Marcos, un desarrollador autodidacta y bastante dedicado cuento con +2 años de experiencia, estoy enfocado en desarrollar aplicaciones web y moviles con javascript, actualmente me encuentro trabajando en <a href='https://www.linkedin.com/company/neurons-agency/' rel='noopener noreferer' target='_blank'>@neurons_agency</a> como desarrollador Front-end semi-senior con react y javascript",
   ROLE: 'Desarrollador Front-end',
   CONTACT_ME: {
-    labels: ['Tu Nombre', 'Correo', '¿Por que me contactas?'],
-    send: 'Enviar',
-    subject: '¡Hola! Necesito comunicarme contigo.',
-    message: (name, email, message) =>
-      `Hola Marcos! \n\n${name} te contacta desde ${email} \n\ny dice lo siguiente: \n${message || '(Mensaje sin cuerpo)'}`,
+    EMAIL: {
+      labels: ['Tu Nombre', 'Correo', '¿Por que me contactas?'],
+      send: 'Enviar',
+      subject: '¡Hola! Necesito comunicarme contigo.',
+      message: (name, email, message) =>
+        `Hola Marcos! \n\n${name} te contacta desde ${email} \n\ny dice lo siguiente: \n${message || '(Mensaje sin cuerpo)'}`,
+    },
+    WS: {
+      title: 'Escribe tu mensaje',
+      placeholder: '¡Dime lo que sea!',
+    },
+    BUTTONS: ['O escribeme por Whatsapp 👉', 'O enviame un mensaje 👉'],
+    warning: 'Debe llenar los campos',
+  },
+  DOWNLOAD_CV: {
+    title: 'Descargar CV',
+    btnEs: 'Descargar (ES)',
+    btnEn: 'Descargar (EN)',
+  },
+  FOOTER: {
+    title: 'Mis Redes',
   },
 };
 
@@ -51,12 +83,15 @@ document.addEventListener('DOMContentLoaded', async function () {
   main.style.display = 'block';
 
   // clear the innerhtml in noscript if javascript is enable
+
   const ns = document.getElementsByTagName('noscript')[0];
   ns.innerText = 'you need to enable javascript to continue browsing this site 🤒';
 
   // check the browser color-scheme
+
   const el = document.getElementsByClassName('slider')[0];
   const hero = document.getElementById('hero');
+  const netImg = document.getElementsByClassName('networks_img');
   const closeBtn = document.getElementsByClassName('close');
 
   if (!localStorage.theme) {
@@ -65,6 +100,8 @@ document.addEventListener('DOMContentLoaded', async function () {
       el.setAttribute('aria-checked', 'true');
       localStorage.theme = 'dark';
       hero.src = '../assets/images/avatar_dark.svg';
+      netImg[0].src = '../assets/icons/linkedin_light.svg';
+      netImg[1].src = '../assets/icons/github_light.svg';
       for (let i = 0; i <= closeBtn.length - 1; i++) {
         closeBtn[i].firstElementChild.setAttribute('src', '../assets/icons/close_icon_dark.svg');
       }
@@ -73,13 +110,17 @@ document.addEventListener('DOMContentLoaded', async function () {
   } else {
     document.documentElement.setAttribute('data-theme', localStorage.theme);
     hero.src = `../assets/images/avatar_${localStorage.theme === 'dark' ? 'dark' : 'light'}.svg`;
+    netImg[0].src = `../assets/icons/linkedin_${localStorage.theme === 'dark' ? 'dark' : 'light'}.svg`;
+    netImg[1].src = `../assets/icons/github_${localStorage.theme === 'dark' ? 'dark' : 'light'}.svg`;
     for (let i = 0; i <= closeBtn.length - 1; i++) {
       closeBtn[i].firstElementChild.setAttribute(
         'src',
         `../assets/icons/close_icon_${localStorage.theme === 'dark' ? 'dark' : 'light'}.svg`
       );
     }
-    document.querySelector('meta[name="theme-color"]').setAttribute('content', `${localStorage.theme === 'dark' ? '#37A71F' : '#167F00'}`);
+    document
+      .querySelector('meta[name="theme-color"]')
+      .setAttribute('content', `${localStorage.theme === 'dark' ? '#37A71F' : '#167F00'}`);
     el.setAttribute('aria-checked', String(localStorage.theme === 'dark'));
   }
 
@@ -116,11 +157,11 @@ document.addEventListener('DOMContentLoaded', async function () {
     document.querySelector('meta[name="theme-color"]').setAttribute('content', `${dataTheme === 'dark' ? '#37A71F' : '#167F00'}`);
   }
 
-  // set flag
+  // Change language Function
+
   const flag = document.getElementById('flag-selected');
   const lng = document.getElementById('lang-selected');
-  const flagpath = `../assets/icons/${getLang === 'es' ? 'es_flag' : 'en_flag'}.svg`;
-  flag.src = flagpath;
+  flag.src = `../assets/icons/${getLang === 'es' ? 'es_flag' : 'en_flag'}.svg`;
 
   lng.innerText = getLang === 'es' ? 'ES' : 'EN';
 
@@ -142,11 +183,11 @@ document.addEventListener('DOMContentLoaded', async function () {
   }
 
   const flagItem = document.getElementsByClassName('lang-item');
-  flagItem[0].addEventListener('click', () => setLang());
-  flagItem[0].addEventListener('keydown', (e) => setLang('en', e));
+  flagItem[0].addEventListener('click', () => (getLang === 'es' ? setLang() : void 0));
+  flagItem[0].addEventListener('keydown', (e) => (getLang === 'es' ? setLang('en', e) : void 0));
 
-  flagItem[1].addEventListener('click', () => setLang('es'));
-  flagItem[1].addEventListener('keydown', (e) => setLang('es', e));
+  flagItem[1].addEventListener('click', () => (getLang === 'es' ? void 0 : setLang('es')));
+  flagItem[1].addEventListener('keydown', (e) => (getLang === 'es' ? void 0 : setLang('es', e)));
 
   function setLang(lng, keyCode) {
     const spaceKeyCode = 32;
@@ -163,55 +204,70 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
   }
 
+  // Home Navbar translations
+
   const navBtns = document.getElementsByClassName('NavButtons');
   for (let i = 0; i <= navBtns.length - 1; i++) {
     navBtns[i].innerText = lang.BUTTONS[i];
   }
 
+  // About Me
+
   const aboutMe = document.getElementById('about-me');
   aboutMe.innerHTML = lang.ABOUT_ME;
+
+  // Technologies
 
   const techTitle = document.getElementsByClassName('modals-title');
   techTitle[0].innerText = lang.BUTTONS[1];
   techTitle[1].innerText = lang.BUTTONS[2];
+
+  // Experience
 
   const workRole = document.getElementsByClassName('work-role');
   for (let i = 0; i <= workRole.length - 1; i++) {
     workRole[i].innerText = lang.ROLE;
   }
 
-  // const swap = document.getElementsByClassName('swap_button');
-  // swap[0].addEventListener('click', () => {
-  //   document.getElementsByClassName('contact_form')[0].removeAttribute('aria-expanded')
-  //   document.getElementsByClassName('contact_ws')[0].setAttribute('aria-expanded', 'true')
-  // })
-  // swap[1].addEventListener('click', () => {
-  //   document.getElementsByClassName('contact_ws')[0].removeAttribute('aria-expanded')
-  //   document.getElementsByClassName('contact_form')[0].setAttribute('aria-expanded', 'true')
-  // })
+  // Contact
+
+  const swap = document.getElementsByClassName('swap_button');
+  swap[0].addEventListener('click', () => {
+    document.getElementsByClassName('contact_form')[0].removeAttribute('aria-expanded');
+    document.getElementsByClassName('contact_ws')[0].setAttribute('aria-expanded', 'true');
+  });
+  swap[1].addEventListener('click', () => {
+    document.getElementsByClassName('contact_ws')[0].removeAttribute('aria-expanded');
+    document.getElementsByClassName('contact_form')[0].setAttribute('aria-expanded', 'true');
+  });
+
+  swap[0].setAttribute('data-message', lang.CONTACT_ME.BUTTONS[0]);
+  swap[1].setAttribute('data-message', lang.CONTACT_ME.BUTTONS[1]);
+
+  // Contact Form Email
 
   const msgLabel = document.getElementsByClassName('message_label');
   for (let i = 0; i <= msgLabel.length - 1; i++) {
-    msgLabel[i].innerText = lang.CONTACT_ME.labels[i];
+    msgLabel[i].innerText = lang.CONTACT_ME.EMAIL.labels[i];
   }
 
   const sendBtn = document.getElementsByClassName('send_button');
-  sendBtn[0].innerText = lang.CONTACT_ME.send;
+  sendBtn[0].innerText = lang.CONTACT_ME.EMAIL.send;
 
-  sendBtn[0].addEventListener('click', sendEmail)
+  sendBtn[0].addEventListener('click', sendEmail);
 
-  function sendEmail () {
+  function sendEmail() {
     const name = document.getElementById('contact_name').value;
     const email = document.getElementById('contact_email').value;
     const message = document.getElementById('contact_message').value;
 
     if (name && email) {
-      const formatedMessage = lang.CONTACT_ME.message(name, email, message);
-      sendEmailFinale(formatedMessage)
+      const formatedMessage = lang.CONTACT_ME.EMAIL.message(name, email, message);
+      sendEmailFinale(formatedMessage);
     } else {
-      alert('missing fields')
+      alert(lang.CONTACT_ME.warning);
     }
-  };
+  }
 
   function sendEmailFinale(message) {
     const myEmail = 'mguerrerofinol';
@@ -222,9 +278,58 @@ document.addEventListener('DOMContentLoaded', async function () {
     link.setAttribute('target', '_blank');
     link.setAttribute('rel', 'noopener noreferrer');
     document.body.appendChild(link);
-    link.click()
-    link.parentNode.removeChild(link)
+    link.click();
+    link.parentNode.removeChild(link);
   }
+
+  // Contact Send Whatsapp
+
+  const sendWsBtn = document.getElementsByClassName('send_ws_button');
+  const wsTitle = document.getElementById('ws_title_msg');
+  const wsTextArea = document.getElementById('ws_contact_message');
+
+  wsTitle.innerHTML = lang.CONTACT_ME.WS.title;
+  wsTextArea.setAttribute('placeholder', lang.CONTACT_ME.WS.placeholder);
+
+  sendWsBtn[0].addEventListener('click', () => proccesMessage());
+
+  function proccesMessage() {
+    const message = wsTextArea.value;
+
+    if (message) {
+      sendWhatsAppMsg(message);
+    } else {
+      alert(lang.CONTACT_ME.warning);
+    }
+  }
+
+  function sendWhatsAppMsg(msg) {
+    const formatedMsg = msg.replace(/\s+/g, '%20');
+    const myNum = '584246773090';
+    const url = `https://wa.me/${myNum}?text=${formatedMsg}`;
+
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('target', '_blank');
+    link.setAttribute('rel', 'noopener noreferrer');
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode.removeChild(link);
+  }
+
+  // Download CV
+
+  const downloadTitle = document.getElementById('download-title');
+  downloadTitle.innerHTML = lang.DOWNLOAD_CV.title;
+
+  const downloadBtns = document.getElementsByClassName('download_button');
+  downloadBtns[0].innerHTML = lang.DOWNLOAD_CV.btnEs;
+  downloadBtns[1].innerHTML = lang.DOWNLOAD_CV.btnEn;
+
+  // Footer
+
+  const footerTitle = document.getElementById('footer_title');
+  footerTitle.innerHTML = lang.FOOTER.title;
 
   const fText = document.getElementById('feature-text');
   carousel(carouselText, fText);
